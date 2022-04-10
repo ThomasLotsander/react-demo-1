9363cf70c72ba8e0a7393f22b28c6f9e86a971d8
Thomas Lotsander <thomas.lotsander@rikstv.no>
2022-04-10 16:52:06 +0200

    Add add task workflow

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import Header from "./components/Header";
 import UnorderdList from "./components/UnorderdList";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = () => {  
   
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -32,6 +34,14 @@ const App = () => {
     },
 ])
 
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1;
+  const newTask = {id, ...task}
+  console.log(...tasks)
+  setTasks([...tasks, newTask])
+
+}
+
 const ToggleReminder = (id) => {
   setTasks(
     tasks.map((task) => 
@@ -45,7 +55,8 @@ const DeleteTask = (id) => {
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? 
         <Tasks tasks={tasks} onDelete={DeleteTask} onToggle={ToggleReminder} /> 
         : 'No tasks to show'
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,14 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
-const Header = ({title}) => {
+const Header = ({ title, onAdd, showAdd }) => {
 
-    const onClick = () => {
-        console.log('Header click')
-    }
+    
+    
 
   return (
     <header className='header'>        
         <h1 style={HeadingStyle}>{title}</h1>  
-        <Button color='green' text='Add' onClick={onClick}/> 
+        <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/> 
     </header>
   )
 }
